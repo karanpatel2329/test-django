5c45de745a0476b9a4f1438f3012b3bb056c333e
Anirudh Jwala <anirudhsai2@gmail.com>
2020-08-31 10:23:35 +0530

    Update urls.py
    Raw string can be omitted

--- api/user/urls.py
+++ api/user/urls.py
@@ -3,10 +3,9 @@ from django.urls import path, include
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'', views.UserViewSet)
+router.register('', views.UserViewSet)
 
 urlpatterns = [
-   
     path('login/', views.signin, name="login"),
     path('logout/<int:id>/', views.signout, name="logout"),
     path('',include(router.urls)),
